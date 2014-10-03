ad22a44f3604ef5f09682480b5c8d21f1d5128a1
Ralph Churchill <ralph.churchill@vitals.com>
2014-10-03 09:24:31 -0500

    Added hex and cleaned up a bunch of stuff

--- babel.js
+++ babel.js
@@ -3,10 +3,20 @@ var OrthographicSymbols = [
 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '~', '.', ' '
 ];
 
+// parse args
+var args = document.location.search.split(',').map(function(e) {
+  return parseInt(e.replace(/[^\d]/, ''));
+});
+var Hexagon = args[0] || 0;
+var Wall    = args[1];
+var Shelf   = args[3];
+var Book    = args[4];
+var Page    = args[5];
+
 function pageHeader() {
   var header = document.createElement('p');
   header.setAttribute('class', 'header');
-  header.innerHTML = '1';
+  header.innerHTML = Page || '~';
   return header;
 }
 
@@ -15,8 +25,8 @@ function page() {
   page.setAttribute('class', 'page');
   for(l = 0; l < 40; l++) {
     var line = '';
-    for(letter = 0; letter < 80; letter++) {
-      line += OrthographicSymbols[Math.floor(Math.random() * OrthographicSymbols.length)];
+    for(ll = 0; ll < 80; ll++) {
+      line += letter();
     }
     var node = document.createElement('P');
     node.appendChild(document.createTextNode(line));
@@ -24,46 +34,49 @@ function page() {
   }
   return page;
 }
-// page number
-// if 1, no prev
-// back to shelf
-// if 410, no next
+
 function pageFooter() {
   var footer = document.createElement('div');
   footer.setAttribute('class', 'footer');
 
   var prev = document.createElement('A');
-  prev.setAttribute('href', 'page.html#');
+  if(Page > 1) {
+    prev.setAttribute('href', 'page.html?' + [Hexagon, Shelf, Book, Page-1].join(','));
+  }
   prev.innerHTML = 'prev';
   footer.appendChild(prev);
 
   var back = document.createElement('A');
-  back.setAttribute('href', 'shelf.html');
+  back.setAttribute('href', 'shelf.html?' + [Hexagon, Shelf, Book].join(','));
   back.innerHTML = 'back';
   footer.appendChild(back);
 
   var next = document.createElement('A');
-  next.setAttribute('href', 'page.html#');
+  if(Page < 410) {
+    next.setAttribute('href', 'page.html?' + [Hexagon, Shelf, Book, Page+1].join(','));
+  }
   next.innerHTML = 'next';
   footer.appendChild(next);
 
   return footer;
 }
-function wall() {
-  var wall = document.createElement('table');
-  for(s = 0; s < 5; s++) {
-    var shelf = document.createElement('tr');
-    for(b = 0; b < 32; b++) {
-      var book = document.createElement('td');
-      var linkToBook = document.createElement('a');
-      linkToBook.setAttribute('href', 'page.html#' + s + ',' + b);
-      linkToBook.innerHTML = 
-        "<span class='rotate book'>" + letter() + "</span>" +
-        "<span class='rotate book'>" + letter() + "</span>" +
-        "<span class='rotate book'>" + letter() + "</span>" +
-        "<span class='rotate book'>" + letter() + "</span>" +
-        "<span class='rotate book'>" + letter() + "</span>";
 
+function wall() {
+  var wall = document.createElement('TABLE');
+  for(s = 0; s < 5; s++) {
+    var shelf = document.createElement('TR');
+    for(b = 0; b < 32; b++) {
+      var book = document.createElement('TD');
+      var linkToBook = document.createElement('A');
+      linkToBook.setAttribute('class', 'spine');
+      linkToBook.setAttribute('href', 'page.html?' + [Hexagon, s, b, 1].join(','));
+      for(i = 0; i < 5; i++) {
+        var spine = document.createElement('SPAN');
+        spine.setAttribute('class',
+          ['book', 'rotate90n'].join(' '));
+        spine.innerHTML = letter();
+        linkToBook.appendChild(spine);
+      }
       book.appendChild(linkToBook);
       shelf.appendChild(book);
     }
@@ -71,6 +84,7 @@ function wall() {
   }
   return wall;
 }
+
 function letter() {
   return OrthographicSymbols[Math.floor(Math.random() * OrthographicSymbols.length)];
 }
